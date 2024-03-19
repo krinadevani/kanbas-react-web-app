3a3413ab3826fed40cb9499f27ee83aca93c7bc8
Krina Devani <krinadevani@Krinas-MacBook-Air.local>
2024-03-19 15:02:43 -0400

    added labs

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,19 +1,23 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, Navigate } from "react-router-dom";
 import Nav from "../Nav";
 import Assignment3 from "./a3";
 import Assignment4 from "./a4";
+import store from "./store";
+import { Provider } from "react-redux";
+
 function Labs() {
    return (
-      <div className="container-fluid">
-         <h1>Labs</h1>
-         <Nav />
-         <Link to="/Labs/a3">Assignment 3</Link> |
-         <Link to="/Labs/a4">Assignment 4</Link>
-         <Routes>
-            <Route path="/a3/*" element={<Assignment3 />} />
-            <Route path="/a4" element={<Assignment4 />} />
-         </Routes>
-      </div>
+      <Provider store={store}>
+         <div className="container-fluid">
+            <h1>Labs</h1>
+            <Nav />
+            <Routes>
+               <Route path="/" element={<Navigate to="a3" />} />
+               <Route path="/a3" element={<Assignment3 />} />
+               <Route path="/a4" element={<Assignment4 />} />
+            </Routes>
+         </div>
+      </Provider>
    );
 }
 export default Labs;
